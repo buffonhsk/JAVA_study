e71014590aab0d4cefb09c1d67053858bd34973c
buffonhsk <buffonhsk@naver.com>
2016-02-29 15:07:34 +0900

    HTML-example

---
+++ workspace/study/fuuny_jQuery/ex/ex3/step1/js/custom.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function(){
+	//.btnMenu클릭시 모션
+	$(".btnMenu").on('click', function(){
+		$(this).fadeOut();
+		$("section").addClass('on');
+		$("nav").addClass('on');
+	});
+	
+	//nav 안에 메뉴 선택시 모션
+	$("nav li").on('click', function(){
+		$(".btnMenu").fadeIn();
+		$("section").removeClass("on");
+		$("nav").removeClass("on");
+		var i = $(this).index();
+		$("section>div").removeClass("on");
+		$("section>div").eq(i).addClass("on");
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
